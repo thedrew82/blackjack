e2dc828d7b03ab04fbb0a960f66b1946e35360e6
Andrew Hise <andrew.hise@noa.nintendo.com>
2018-05-08 08:14:29 -0700

    split into discrete test classes

--- test_blackjack.py
+++ test_blackjack.py
@@ -2,58 +2,67 @@ import unittest
 import blackjack
 
 
-class TestBlackJack(unittest.TestCase):
+class TestBlackJackCard(unittest.TestCase):
 
-    def test_card_str(self):
+    def test_str(self):
         text = 'Two of Hearts'
         result = str(blackjack.Card('Hearts', 'Two'))
         self.assertEqual(result, text)
 
-    def test_card_value(self):
+    def test_value(self):
         value = 4
         card = blackjack.Card('Diamonds', 'Four')
         self.assertEqual(card.value, value)
 
-    def test_deck_init(self):
+
+class TestBlackJackDeck(unittest.TestCase):
+
+    def test_init(self):
         self.assertEqual(len(blackjack.Deck()), 52)
 
-    def test_deck_shuffle(self):
+    def test_shuffle(self):
         test_deck = blackjack.Deck()
         self.assertIsNone(test_deck.shuffle())
 
-    def test_deck_deal(self):
+    def test_deal(self):
         test_deck = blackjack.Deck()
         result = str(test_deck.deal())
         text = 'Ace of Clubs'
         self.assertEqual(result, text)
 
-    def test_hand_add_card(self):
+
+class TestBlackJackHand(unittest.TestCase):
+
+    def test_add_card(self):
         test_hand = blackjack.Hand()
         test_hand.cards = ['Two of Hearts', 'Ace of Spades']
         test_hand.add_card('King of Clubs')
         result = ['Two of Hearts', 'Ace of Spades', 'King of Clubs']
         self.assertEqual(result.sort(), test_hand.cards.sort())
 
-    def test_hand_check_ace(self):
+    def test_check_ace(self):
         test_hand = blackjack.Hand()
         test_hand.cards = ['Ace of Hearts', 'Ace of Spades']
         test_hand.check_for_ace()
         self.assertEqual(2, test_hand.aces)
 
-    def test_hand_adjust_ace(self):
+    def test_adjust_ace(self):
         test_hand = blackjack.Hand()
         test_hand.value = 31
         test_hand.adjust_for_ace()
         self.assertEqual(21, test_hand.value)
 
-    def test_chips_win_bet(self):
+
+class TestBlackJackChips(unittest.TestCase):
+
+    def test_win_bet(self):
         test_chips = blackjack.Chips()
         test_chips.total = 100
         test_chips.bet = 20
         test_chips.win_bet()
         self.assertEqual(120, test_chips.total)
 
-    def test_chips_lose_bet(self):
+    def test_lose_bet(self):
         test_chips = blackjack.Chips()
         test_chips.total = 100
         test_chips.bet = 30
