a1b168e7927c30be5ef8d77fb72e16137e2d13e4
Andrew Hise <ahise82@gmail.com>
2018-05-24 22:30:36 -0700

    add more methods

--- blackjack.py
+++ blackjack.py
@@ -10,7 +10,9 @@ def load_cards(file):
         v = data['values']
         return s, r, v
 
+
 (suits, ranks, values) = load_cards('house.yaml')
+
 
 class Card:
 
@@ -48,8 +50,11 @@ class Hand:
 
     def __init__(self):
         self.cards = []
-        self.value = 0
         self.aces = 0
+        self.value = 0
+
+    def __str__(self):
+        return ', '.join(str(x) for x in self.cards)
 
     def add_card(self, card):
         self.cards.append(card)
@@ -77,5 +82,73 @@ class Chips:
         self.total = self.total - self.bet
 
 
+def take_bet(total):
+    while True:
+        try:
+            b = int(input(f'Enter your bet: [max {total}] '))
+            if b <= total:
+                return b
+            else:
+                raise ValueError()
+        except ValueError:
+            print('Invalid bet, please bet again.\n')
+            continue
+
+
+def hit(deck, hand):
+    hand.add_card(deck.deal())
+
+
+def get_hand_value(hand):
+    hand.value = 0
+    for card in hand.cards:
+        hand.value += card.value
+
+
+def hit_or_stand(deck, hand):
+    global playing
+
+    try:
+        action = input(f'Hit or Stand? [H/s]\n').lower()
+        if action == 'hit' or action == 'h':
+            hit(deck, hand)
+            playing = True
+        elif action == 'stand' or action == 's':
+            playing = False
+        elif not action:
+            hit(deck, hand)
+        else:
+            raise ValueError()
+    except ValueError:
+        print('Invalid action, please try again.\n')
+
+
 if __name__ == '__main__':
-    pass
+    my_deck = Deck()
+    my_deck.shuffle()
+    my_chips = Chips()
+    my_chips.total = 100
+    my_hand = Hand()
+    my_hand.add_card(my_deck.deal())
+    my_hand.add_card(my_deck.deal())
+    while True:
+        take_bet(my_chips.total)
+        get_hand_value(my_hand)
+        print(my_hand)
+        print(my_hand.value)
+        hit_or_stand(my_deck, my_hand)
+
+        while playing:
+            hit_or_stand(my_deck, my_hand)
+            get_hand_value(my_hand)
+            print(my_hand)
+            print(my_hand.value)
+
+        if my_hand.value == 21:
+            print('WIN!')
+        elif my_hand.value > 21:
+            print('BUST!')
+        else:
+            print(my_hand.value)
+
+        break
